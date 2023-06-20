11d7d07917303d171c82732674e30e5233858a7b
Heather Turner <ht@heatherturner.net>
2023-06-20 16:09:02 +0100

    document animal_sounds

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,13 @@
+#' Create Animal Sounds Message
+#'
+#' @param animal the name of the animal (character)
+#' @param sound the sound of the animal (character)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' animal_sounds("cow", "moo")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
