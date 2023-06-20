fe7e9c9fee2663fc8316f650a2b80abe933aa7c7
Heather Turner <ht@heatherturner.net>
2023-06-20 16:57:25 +0100

    add links; add rlang and cli import

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,5 +1,9 @@
 #' Create Animal Sounds Message
 #'
+#' Create a message stating what sound an animal makes.
+#'
+#' We have used [paste0()] but not [glue::glue()]
+#'
 #' @param animal the name of the animal (character)
 #' @param sound the sound of the animal (character)
 #'
@@ -9,7 +13,8 @@
 #' @examples
 #' animal_sounds("cow", "moo")
 animal_sounds <- function(animal, sound) {
-  stopifnot(is.character(animal) & length(animal) == 1)
-  stopifnot(is.character(sound) & length(sound) == 1)
+  if (!rlang::is_character(sound, n = 1)) {
+    cli::cli_abort("`sound` must be a single string!")
+  }
   paste0("The ", animal, " says ", sound, "!")
 }
